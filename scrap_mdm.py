a1bff6dbfb3519d120df0f272407446c8d28afcf
titi33ledev <tidiane.hubert.bordeaux@gmail.com>
2023-06-18 02:14:15 +0200

    création de la fonctions scrap_product_url

--- scrap_mdm.py
+++ scrap_mdm.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.by import By
 import pandas as pd
 
 #%%
-def scrap(mot_cles):
+def scrap_first_page(mot_cles):
     
     driver = webdriver.Chrome()
     driver.get("https://www.maisonsdumonde.com/")
@@ -25,37 +25,109 @@ def scrap(mot_cles):
     search_bar.send_keys(Keys.RETURN)
 
     time.sleep(random.randint(10, 35))
-
+    driver.implicitly_wait(20)
+    
+    scrolling_bar = driver.find_element(By.XPATH,'/html/body/div[1]/main/section/div/div/section/div[3]/div[3]/div/div/div/button')
+    
+    while True: 
+        
+        try:
+            
+            scrolling_bar.click()
+            driver.implicitly_wait(3)
+            
+        except:
+            
+            break
+                
     page_source = driver.page_source
+    
     return BeautifulSoup(page_source, 'html.parser')
+
+
+#%%
+
+def scrap_data_products(soup):
+    
+    
 # %%
 def process(soup):
     
-    number_of_products = []
     titles = []
     prices = []
+    product_urls = []
     
-    nb_products_divs = soup.find_all({"class" : "text-primary font-regular ds-body-m"})
-    titles_divs = soup.find_all({"class" : "text-primary font-regular ds-body-m mb-2 h-[3em] line-clamp-2 after:absolute after:inset-0"})
-    prices_divs = soup.find_all({"class" : "text-primary-accent font-semibold ds-body-m inline-block"})
+    titles_divs = soup.find_all('h2', class_='text-primary font-regular ds-body-m mb-2 h-[3em] line-clamp-2 after:absolute after:inset-0')
+    prices_divs = soup.find_all('p', class_='text-primary-accent font-semibold ds-body-m inline-block')
+    brand_divs = soup.find_all('p', attrs={'data-el': 'product-card__brand'})
+
     
-    for nb_products_div, titles_div, prices_div in zip(nb_products_divs,titles_divs,prices_divs):
-        
-        nb_products = nb_products_div.get_text().strip()
+    brand_divs = soup.find_all('p', attrs={'data-el': 'product-card__brand'})
+    
+    for brand_div in brand_divs:
+        link_div = brand_div.find_next('a', attrs={'data-el': 'resolver-link'})
+        if link_div is not None:
+            product_url = link_div['href']
+            product_urls.append(product_url)
+    
+    
+    
+    for titles_div, prices_div, product_url in zip(titles_divs, prices_divs, product_urls):
         price = prices_div.get_text().strip().replace('\u202f', '').replace('€', '').replace(',', '.') if prices_div else "NaN"
         title = titles_div.get_text().strip()
-        
+
         titles.append(title)
         prices.append(price)
-        number_of_products.append(nb_products)
 
 
     df = pd.DataFrame(
         {'titres' : titles,
         'prix' : prices,
-        'nombres_de_produits': number_of_products,
+        'url_produits' : product_url
         }
     )
     
     return df
+# %%
+def scrap_product_url(df,nom_colonne='url_produits'):
+    
+    nb_images = []
+    descriptions = []
+    uri = "https://www.maisonsdumonde.com/"
+    
+    if nom_colonne not in df.columns:
+        print(f"La colonne '{nom_colonne}' n'existe pas dans le DataFrame.")
+        return None
+    
+    for index, row in df.iterrows():
+        driver = webdriver.Chrome()
+        lien = uri + row['url_produits']    
+        driver.get(lien)
+        
+        time.sleep(random.randint(10,20))
+        
+        button = driver.find_element(By.XPATH, '//*[@id="footer_tc_privacy_button_2"]')
+        button.click()
+        
+        page_source = driver.page_source
+        
+        driver.implicitly_wait(7)
+        
+        pagination_buttons = soup.find_all('button', attrs={'data-el': 'ds-pager__bullet'})
+        nombre_images = len(pagination_buttons)        
+        product_description = soup.find('div', attrs={'data-v-cd98ad86': '', 'data-el': 'product-description-panel__rich-text'})
+
+        nb_images.append(nombre_images)
+        descriptions.append(product_description)
+        
+        
+    df = pd.DataFrame(
+    {"nombres d'images" : nb_images,
+        "descriptions" : descriptions
+        }
+    )
+    
+    return df
+        
+        
 # %%
